2f4832cac504d05257975b15b35778bd58afa58f
DESKTOP-Chicago\twill <twill14@gmail.com>
2020-02-14 13:59:15 -0600

    pytest.ini

--- tests/tests_television.py
+++ tests/tests_television.py
@@ -1,5 +1,15 @@
 from pytest import mark
 
-@mark.parametrize
+
+@mark.parametrize('tv_brand', [
+    'Samsung',
+    'Sony',
+    'Vizio'
+    ]
+)
 def test_television_turns_on(tv_brand):
     print(f"{tv_brand} turns on as expected")
+
+
+def test_browser_can_navigate_to_training_ground(browser):
+    browser.get('http://techstepacademy.com/training-ground')
