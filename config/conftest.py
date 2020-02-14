fc2f9be64723716cc4ab83ff6cd23043bee0b250
DESKTOP-Chicago\twill <twill14@gmail.com>
2020-02-14 14:08:14 -0600

    test data and params

---
+++ config/conftest.py
@@ -0,0 +1,26 @@
+import json
+
+from pytest import fixture
+from selenium import webdriver
+
+data_path = 'test_data.json'
+
+
+def load_test_data(path):
+    with open(path) as data_file:
+        data = json.load(data_file)
+        return data
+
+
+@fixture(params=[webdriver.Chrome, webdriver.Firefox, webdriver.Edge])
+def browser(request):
+    driver = request.param
+    drvr = driver()
+    yield drvr
+    drvr.quit()
+
+
+@fixture(params=load_test_data(data_path))
+def test_data(request):
+    data = request.param
+    return data
